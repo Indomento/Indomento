afeb7cd4987397e2b309ccdee78dc7de912cd376
vania91991 <vania.kotseva@hotmail.com>
2016-08-30 16:21:45 +0300

    Update index.php

--- blog/views/home/index.php
+++ blog/views/home/index.php
@@ -1,17 +1,19 @@
 <main>
-    <?php $this->title = 'Welcome'; ?>
+    <?php $this->title = 'Добре дошли'; ?>
 
-    <h1><?=htmlspecialchars($this->title)?></h1>
+    <h1><?= htmlspecialchars($this->title) ?></h1>
 
     <?php foreach ($this->posts as $post) : ?>
-        <h1><?=htmlentities($post['title']) ?></h1>
-        <p>
-            <i>Posted on</i>
-            <?=htmlentities($post['date']) ?>
-            <i>by</i>
-            <?=htmlentities($post['full_name']) ?>
-        </p>
-        <p><?=$post['content'] ?></p>
+        <h2 class="title"><?= htmlentities($post['title']) ?></h2>
+        <div class="date">
+            <i>от</i> <?= htmlentities($post['full_name']) ?>
+            <i>в</i><?= (new DateTime($post['date']))->format('H:i') ?>
+            <i>на</i> <?= (new DateTime($post['date']))->format('d.m.Y') ?>
+        </div>
+        <p class="content"><?= $post['content'] ?></p>
     <?php endforeach ?>
 </main>
+
+
+
 
