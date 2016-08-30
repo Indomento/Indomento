3bf7a14768397b959ed40706954b69b44fe1a048
vania91991 <vania.kotseva@hotmail.com>
2016-08-30 18:17:36 +0300

    Update register.php

--- blog/views/users/register.php
+++ blog/views/users/register.php
@@ -1,5 +1,9 @@
-<?php $this->title = 'Register New User'; ?>
-
+<?php $this->title = 'Регистрация'; ?>
 <h1><?= htmlspecialchars($this->title) ?></h1>
-
-<!-- TODO: register form will come here ... -->
+<form method="post">
+    <div>Потребителско име <input type="text" name="username" required/></div>
+    <div>Име <input type="text" name="full_name"/></div>
+    <div>Парола <input type="password" name="password" required/></div>
+    <div>Повтори паролата <input type="password" name="password_confirm" required/></div>
+    <div><input type="submit" value="Регистрация"/>
+</form>
