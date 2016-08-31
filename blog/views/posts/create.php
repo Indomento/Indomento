fa54e891b9e774ab4c367e5c8c813e0dbfaa576f
vania91991 <vania.kotseva@hotmail.com>
2016-09-01 00:48:52 +0300

    Update create.php

--- blog/views/posts/create.php
+++ blog/views/posts/create.php
@@ -1,12 +1,15 @@
-<?php $this->title = 'Create new post'; ?>
+<?php $this->title = 'Създаване на нова публикация'; ?>
 
 <h1><?=htmlspecialchars($this->title)?></h1>
 
-<form method="post">
-    <div>Title:</div>
-    <input type="text" name="post_title" />
-    <div>Content:</div>
+<form method="post" > <br/>
+    <div>Заглавие:</div>
+    <input type="text" class="form-control" name="post_title" />
+    <br/>
+    <div>Текст:</div>
     <textarea rows=10 name="post_content"></textarea>
-    <div><input type="submit" value="Create"/>
-    <a href="<?=APP_ROOT?>/posts">[Cancel]</a></div>
+    <div>
+        <input type="submit" value="Създай" class="btn btn-success"/>
+        <a class="btn btn-danger" href="<?=APP_ROOT?>/posts">Отказ</a>
+    </div>
 </form>
