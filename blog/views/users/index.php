ee9d8c2873f4c85bf9ef778f2953c55eff5548d1
Stanislav Georgiev <stanislavgeorgiev.sg@gmail.com>
2016-08-31 11:02:44 +0300

    Create index.php

---
+++ blog/views/users/index.php
@@ -0,0 +1,22 @@
+<?php $this->title = 'Users'; ?>
+
+<h1><?=htmlspecialchars($this->title) ?></h1>
+
+<table>
+	<tr>
+		<th>ID</th>
+		<th>Потребителско име</th>
+		<th>Име</th>
+	</tr>
+
+	<?php foreach ($this->users as $user) : ?> 
+	<tr>
+		<td><?= $user['id'] ?></td>
+		<td><?= htmlspecialchars($user['username']) ?></td>
+		<td><?= htmlspecialchars($user['full_name']) ?></td>
+	</tr>
+<?php endforeach ?>	
+
+</table>
+
+
