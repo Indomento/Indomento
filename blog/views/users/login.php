1ec486924bcff54da43f5921548f26a1650c17e5
Stanislav Georgiev <stanislavgeorgiev.sg@gmail.com>
2016-08-31 09:49:37 +0300

    Update login.php

--- blog/views/users/login.php
+++ blog/views/users/login.php
@@ -2,4 +2,18 @@
 
 <h1><?= htmlspecialchars($this->title) ?></h1>
 
-<!-- TODO: login form will come here ... -->
+<form method="post">
+	<div>
+		<label for="username">Username</label>
+	</div>
+	<div>
+		<input type="text" name="username" required />
+	</div>
+	<div>
+		<label for="password">Password</label>
+	</div>
+	<div>
+		<input type="password" name="password" required />
+	</div>
+	<div><input type="submit" value="Login"/></div>
+</form>
