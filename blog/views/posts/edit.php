d2afb0afd7efc5154c3bb0d06d748529663ce550
Stanislav Georgiev <stanislavgeorgiev.sg@gmail.com>
2016-09-01 07:50:04 +0300

    Update edit.php

--- blog/views/posts/edit.php
+++ blog/views/posts/edit.php
@@ -1,15 +1,16 @@
 <?php $this->title = 'Редактирай публикация'; ?>
 
-<h1><?= htmlspecialchars($this->title) ?></h1>
+<h1><?=htmlspecialchars($this->title)?></h1>
 
-<form method="post"><br>
+<form method="post">
     <div>Заглавие:</div>
-    <input type="text" name="post_title" value="<?= htmlspecialchars($this->post['title']) ?>"/>
-    <div>Текст:</div>
-    <textarea rows="10" name="post_content"><?= htmlspecialchars($this->post['content']) ?></textarea><br>
-    <input type="text" name="post_date" value="<?= htmlspecialchars($this->post['date']) ?>"/>
+    <input type="text" name="post_title" value="<?=htmlspecialchars($this->post['title'])?>" />
+    <div>Съдържание:</div>
+    <textarea rows="10" name="post_content"><?=htmlspecialchars($this->post['content'])?></textarea>
+    <div>Date:</div>
+    <input type="text" name="post_date" value="<?=htmlspecialchars($this->post['date'])?>"/>
     <div>Author ID:</div>
-    <input type="text" name="user_id" value="<?= htmlspecialchars($this->post['user_id']) ?>"/><br/>
+    <input type="text" name="user_id" value="<?=htmlspecialchars($this->post['user_id'])?>"/>
     <div><input type="submit" value="Редактирай">
-        <a class="btn btn-danger" href="<?= APP_ROOT ?>/posts">Отказ</a>
+    <a href="<?=APP_ROOT?>/posts">Cancel</a></div>
 </form>
